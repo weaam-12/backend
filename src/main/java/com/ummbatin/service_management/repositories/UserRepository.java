0635d4984d832efb980641b84a79a0980794ebd0
weaam-12 <abukafweaam2001@gmail.com>
2025-07-23 16:35:57 +0300

    fix readingnmm

--- src/main/java/com/ummbatin/service_management/repositories/UserRepository.java
+++ src/main/java/com/ummbatin/service_management/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.ummbatin.service_management.repositories;
 
 import com.ummbatin.service_management.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {  // Long matches User's id
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    List<User> findAllWithProperties();
-}
+    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.properties WHERE u.properties IS NOT EMPTY")
+    List<User> findAllWithProperties();}
