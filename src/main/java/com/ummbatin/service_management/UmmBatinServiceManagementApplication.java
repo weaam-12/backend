58388aac8875e6db71dfec4b3db4ab300face5a7
weaam-12 <abukafweaam2001@gmail.com>
2025-07-24 10:33:54 +0300

    fix reading

--- src/main/java/com/ummbatin/service_management/UmmBatinServiceManagementApplication.java
+++ src/main/java/com/ummbatin/service_management/UmmBatinServiceManagementApplication.java
@@ -2,16 +2,28 @@ package com.ummbatin.service_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UmmBatinServiceManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmmBatinServiceManagementApplication.class, args);
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 
 	}
 
+	@Configuration
+	public class StaticResourceConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry
+					.addResourceHandler("/uploads/**")
+					.addResourceLocations("file:uploads/");
+		}
+	}
 }
